fee8fb2c8ec928fa6c4e15a1085b1df4101ecde8
huy90532056 <huytruong90532056@gmail.com>
2024-01-27 20:04:05 +0700

    update

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Mycomponent extends React.Component {
+  render() {
+    return (
+      <div>
+        My first Component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default Mycomponent;
